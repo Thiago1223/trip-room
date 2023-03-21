682ecfe36165a4bb92d64613e453ceb302b69ebd
THIAGO FREITAS VILARIÇO <22282213@dsn127m1242268.cfp127aluno.local>
2023-03-21 16:49:34 -0300

    atualizado

--- app/src/main/java/br/senai/sp/jandira/triproom/HomeActivity.kt
+++ app/src/main/java/br/senai/sp/jandira/triproom/HomeActivity.kt
@@ -8,10 +8,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Icon
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -42,67 +39,111 @@ fun HomeScreen() {
          modifier = Modifier
              .fillMaxSize()
     ) {
-        Box(
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(197.dp)
+        Column(
+            modifier = Modifier.fillMaxSize()
         ) {
-            Image(
-                painter = painterResource(id = R.drawable.paris),
-                contentDescription = ""
-            )
-            Column(
+            Box(
                 modifier = Modifier
-                    .height(197.dp),
-                verticalArrangement = Arrangement.Bottom
+                    .fillMaxWidth()
+                    .height(220.dp)
             ) {
-                Row(
-                    verticalAlignment = Alignment.CenterVertically,
-                    modifier = Modifier.padding(horizontal = 20.dp)
+                Image(
+                    painter = painterResource(id = R.drawable.paris),
+                    contentDescription = ""
+                )
+                Column(
+                    modifier = Modifier
+                        .height(197.dp),
+                    verticalArrangement = Arrangement.Bottom
                 ) {
-                    Icon(
-                        painter = painterResource(id = R.drawable.baseline_location_on_24),
-                        contentDescription = "",
-                        modifier = Modifier.size(16.dp),
-                        tint = Color.White
-                    )
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                        modifier = Modifier.padding(horizontal = 20.dp)
+                    ) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.baseline_location_on_24),
+                            contentDescription = "",
+                            modifier = Modifier.size(16.dp),
+                            tint = Color.White
+                        )
+                        Text(
+                            text = "You're in Paris",
+                            fontSize = 14.sp,
+                            color = Color.White
+                        )
+                    }
                     Text(
-                        text = "You're in Paris",
-                        fontSize = 14.sp,
-                        color = Color.White
+                        text = "My Trips",
+                        fontSize = 24.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = Color.White,
+                        modifier = Modifier.padding(horizontal = 36.dp)
                     )
                 }
-                Text(
-                    text = "My Trips",
-                    fontSize = 24.sp,
-                    fontWeight = FontWeight.Bold,
-                    color = Color.White,
-                    modifier = Modifier.padding(horizontal = 36.dp)
-                )
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp),
+                    horizontalAlignment = Alignment.End
+                ) {
+                    Surface(
+                        shape = CircleShape,
+                        border = BorderStroke(2.dp, Color.White),
+                        modifier = Modifier.padding(bottom = 3.dp)
+                    ) {
+                        Image(painter = painterResource(
+                            id = R.drawable.susanna_profile),
+                            contentDescription = "",
+                            modifier = Modifier.size(60.dp)
+                        )
+                    }
+                    Text(
+                        text = "Susanna Hoffs",
+                        fontSize = 12.sp,
+                        color = Color.White,
+                        fontWeight = FontWeight.Light
+                    )
+                }
             }
             Column(
                 modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(16.dp),
-                horizontalAlignment = Alignment.End
+                    .fillMaxSize()
+                    .padding(start = 18.dp)
             ) {
-                Surface(
-                    shape = CircleShape,
-                    border = BorderStroke(2.dp, Color.White),
-                    modifier = Modifier.padding(bottom = 3.dp)
-                ) {
-                    Image(painter = painterResource(
-                        id = R.drawable.susanna_profile),
-                        contentDescription = "",
-                        modifier = Modifier.size(60.dp)
-                    )
-                }
                 Text(
-                    text = "Susanna Hoffs",
-                    fontSize = 12.sp,
-                    color = Color.White,
-                    fontWeight = FontWeight.Light
+                    text = "Categories",
+                    fontSize = 14.sp
                 )
+
+                Spacer(modifier = Modifier.height(8.dp))
+
+                Row(
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Card(
+                        modifier = Modifier
+                            .height(64.dp)
+                            .width(109.dp),
+                        backgroundColor = Color(207, 6, 240, 255)
+                    ) {
+                        Column(
+                            modifier = Modifier.fillMaxSize(),
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                            verticalArrangement = Arrangement.Center
+                        ) {
+                            Image(
+                                painter = painterResource(id = R.drawable.mountains),
+                                contentDescription = "",
+                                modifier = Modifier.size(32.dp)
+                            )
+                            Text(
+                                text = "Montain",
+                                fontSize = 14.sp,
+                                color = Color.White
+                            )
+                        }
+                    }
+                }
             }
         }
     }
---
+++ app/src/main/java/br/senai/sp/jandira/triproom/model/Categorie.kt
@@ -0,0 +1,13 @@
+package br.senai.sp.jandira.triproom.model
+
+import androidx.compose.ui.graphics.painter.Painter
+
+data class Categorie (
+
+    var id: Int = 0,
+    var image: Painter? = null,
+    var name: String = ""
+
+) {
+
+}
